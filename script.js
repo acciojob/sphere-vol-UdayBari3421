8228c57ad9bfb25c4e474d8133d3b12c61553b24
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-23 22:17:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+    event.preventDefault(); // Prevent the form from submitting and page refreshing
+    
+    // Get the value of the radius input field
+    var radius = parseFloat(document.getElementById("radius").value);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    // Check if the input is a valid number
+    if (!isNaN(radius)) {
+        // Calculate the volume of the sphere
+        var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+        // Update the volume input field with the calculated value
+        document.getElementById("volume").value = volume.toFixed(2); // Rounded to 2 decimal places
+    } else {
+        // If the input is not a valid number, display an error message
+        document.getElementById("volume").value = "Invalid Input";
+    }
+}
+
+window.onload = function () {
+    // Add an event listener to the form submit event
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
