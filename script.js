1034af35828cff6893e9e93890538547374598b4
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-25 09:57:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
- function volume_sphere() {
-// Get the radius value from the input field
+function volume_sphere() {
 var radius = document.getElementById("radius").value;
 
-// Check if the entered value is a valid number
 if (isNaN(radius) || radius === "") {
 	alert("Please enter a valid number for the radius.");
 	return;
 }
 
-// Calculate the volume of the sphere
 var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
-// Display the volume in the result field
 document.getElementById("volume").value = volume.toFixed(2);
 }
 
 window.onload = function () {
-// Assign the volume_sphere function to the onclick event of the "Calculate" button
 document.getElementById("submit").onclick = volume_sphere;
 };
